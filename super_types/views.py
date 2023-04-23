aa0692c5102d325a787142cf517720e2b174cc2b
nam3dan <namillennial@gmail.com>
2023-04-23 16:36:12 -0500

    Created views CRUD commands for SuperTypes and Super in respective views files.

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import SuperType
+from .serializers import SuperTypeSerializer
+
+@api_view(['GET','POST'])
+def supers_list(request):
+    if request.method == "GET":
+        queryset = SuperType.objects.all()
+        serializer = SuperTypeSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = SuperTypeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['GET','PUT','DELETE'])
+def super_detail(request, pk):
+    supertype = get_object_or_404(SuperType, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperTypeSerializer(supertype);
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = SuperTypeSerializer(supertype, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        supertype.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Super
+from .serializers import SuperSerializer
 
-# Create your views here.
+@api_view(['GET','POST'])
+def supers_list(request):
+    if request.method == "GET":
+        super_type = request.query_params.get('super_type')
+        print(super_type)
+        queryset = Super.objects.all()
+        if super_type:
+                queryset = queryset.filter(super_type__name = super_type)
+        serializer = SuperSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['GET','PUT','DELETE'])
+def super_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super);
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
