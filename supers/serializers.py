d5d3e65170d45605a35637429d43269914ec90f5
nam3dan <namillennial@gmail.com>
2023-04-23 16:55:12 -0500

    Updated URL issue to properly display data + Fixed a serializer mistake + seeded supers database with 5 heros & villains

--- supers/serializers.py
+++ supers/serializers.py
@@ -3,6 +3,6 @@ from .models import Super
 
 class SuperSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Car
+        model = Super
         fields = ['name','alter_ego','primary_ability','secondary_ability','catchprase','super_type']
         depth = 1
